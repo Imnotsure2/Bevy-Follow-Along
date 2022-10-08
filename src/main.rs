91c61ca365c9584c84c4a2f8bfc2bf7989fa5741
Celeste <stampy9999@gmail.com>
2022-10-08 17:36:00 -0500

    Plugins, Resources

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 use bevy::prelude::*;
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_people)
+            .add_system(greet_people);
+    }
+}
 
 fn main() {
     App::new()
-        .add_startup_system(add_people)
-        .add_system(hello_world)
-        .add_system(greet_people)
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
         .run();
 }
-
-fn hello_world() {
-    println!("hello world!");
-}
-
 #[derive(Component)]
 struct Person;
 
@@ -24,8 +27,13 @@ fn add_people(mut commands: Commands) {
     commands.spawn().insert(Person).insert(Name("Zayna Nieves".to_string()));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("hello {}!", name.0);
+struct GreetTimer(Timer);
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("hello {}!", name.0);
+        }
     }
 }
+
